788ce0268ce73530a761452d79c27aacffe20f2f
Carpe-frite <88053163+Carpe-frite@users.noreply.github.com>
2025-09-08 12:16:34 +0200

    First commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String Hallo() {
+	return "Hallo";
+	}
+	
+	@GetMapping("/admin")
+	public String HalloAdmin() {
+	return "Hallo Admin!";
+	}
+}
